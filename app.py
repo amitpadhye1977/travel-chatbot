8f68e59b34507414ffc49a5376a2c6e77bc5a0fe
amitpadhye1977 <amit.padhye@gmail.com>
2025-08-16 22:38:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def search_trips(keyword):
     conn = get_db_connection()
     cur = conn.cursor(dictionary=True)
     query = """
-        SELECT id, DISTINCT('trip_name'), details, cost, duration, trip_date
+        SELECT DISTINCT id, trip_name, details, cost, duration, trip_date
         FROM trips
         WHERE trip_name LIKE %s OR details LIKE %s OR duration LIKE %s
            OR CAST(cost AS CHAR) LIKE %s OR CAST(trip_date AS CHAR) LIKE %s

d6ba8ccf1df86dfcaba0cdcfc87fd0d1bd143d8e
amitpadhye1977 <amit.padhye@gmail.com>
2025-08-16 07:26:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def get_trips():
         conn = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
             user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASS"),
+            password=os.getenv("DB_PASSWORD"),
             database=os.getenv("DB_NAME")
         )
         cursor = conn.cursor()

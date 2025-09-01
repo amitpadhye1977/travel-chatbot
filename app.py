aae1fffe322c8f433dfcec40c74df85f04bdccbf
amitpadhye1977 <amit.padhye@gmail.com>
2025-09-01 10:06:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -207,7 +207,7 @@ def answer_with_openai(user_message, trips):
     system_prompt = (
         "You are a helpful travel assistant for Ashtavinayak Trips organised by Ashtavinayak Dot Net. Detected language: {lang}"
         "Answer smartly and like a customer support executive using the provided trips catalog. Detected language: {lang} "
-        "If Hotel or Photo is asked by user, search for the hotel name in google business and share photo link in reply "
+        "If reviews of Hotel Suresh Inn or Hotel Jayshree Executive is asked by user, search for the hotel name in google reviews and share clickable link in chatbox "
         "If something isn't in the catalog, answer relevant information about Ashtavinayak Tour and Ashtavinayak Dot Net company. If any question related to Ashtavinayak Dot Net Travels as a company and its owner name, mobile, email needs to be fetched from www.ashtavinayak.net website and displayed exactly as fetched. Detected language: {lang} "
         "If unsure, say Please check the official website www.ashtavinayak.net for the latest details. Detected language: {lang}"
     )
